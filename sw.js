00350a06a20f767b506733a72620f0cd74f5f8fb
waritnangame <waritnan09073@gmail.com>
2026-08-27 17:32:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,84 +1,45 @@
-const CACHE_NAME = "wifi-login-v1";
+const CACHE_NAME = "wifi-login-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./logo.PNG",
+  "./wifi-qr.JPG",
   "./manifest.json"
 ];
 
-
-/* =========================
-   ติดตั้ง Service Worker
-========================= */
-
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
-    caches.open(CACHE_NAME)
-      .then(cache => {
-
-        return cache.addAll(
-          FILES_TO_CACHE
-        );
-
-      })
-
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 
   self.skipWaiting();
 });
 
-
-/* =========================
-   เปิดใช้งาน Service Worker
-========================= */
-
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-
-    caches.keys()
-      .then(keys => {
-
-        return Promise.all(
-
-          keys
-            .filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-
-        );
-
-      })
-
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
   );
 
   self.clients.claim();
 });
 
-
-/* =========================
-   ระบบโหลดไฟล์แบบ Offline
-========================= */
-
 self.addEventListener("fetch", event => {
-
   event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) {
+        return cached;
+      }
 
-    caches.match(event.request)
-      .then(cachedResponse => {
-
-        if (cachedResponse) {
-
-          return cachedResponse;
-
-        }
-
-        return fetch(event.request);
-
-      })
-
+      return fetch(event.request);
+    })
   );
-
 });
